c94766a80ef58712db8ffb2bb265eb66759954a7
Frelon <lixuanliming@gmail.com>
2019-11-06 18:16:18 +0800

    fatree

--- fatree.py
+++ fatree.py
@@ -11,13 +11,14 @@ def simpleTest():
     "Create and test a simple network"
     topo = MyTopo()
     net = Mininet( topo, controller=RemoteController, host=CPULimitedHost, link=TCLink )
-    net.addController( 'c0', controller=RemoteController, ip='10.0.0.20', port=6633 )
+    net.addController( 'c0', controller=RemoteController, ip='172.17.151.6', port=6633 )
     net.start()
-    print "Dumping host connections"
+    print ("Dumping host connections")
     dumpNodeConnections(net.hosts )
-    print "Testing network connectivity"
+    print ("Testing network connectivity")
     net.pingAll()
-    print "Testing bandwidth between h1 with h2, h3, h5"
+    print ("Testing bandwidth between h1 with h2, h3, h5")
+    
     h1, h2 = net.get( 'h1', 'h2' )
     net.iperf( ( h1, h2 ) )
     h1, h3 = net.get( 'h1', 'h3' )
