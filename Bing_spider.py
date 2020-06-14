3d7f3bbc6b4767793b7e629bf1245859dc17ae1e
lixuanliming <lixuanliming@gmail.com>
2020-06-14 21:34:18 +0800

    spider

---
+++ Bing_spider.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+import requests
+from lxml import etree
+import os
+webPage = requests.get(url = "https://cn.bing.com/?FORM=BEHPTB&ensearch=1",
+        headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"}
+        )
+we = webPage.text
+ssr = etree.HTML(we)
+Links = ssr.xpath("//div/div[@id='bgDiv']/div/@data-ultra-definition-src")
+Desc=ssr.xpath("//a/div/h2/text()")
+
+Str="https://cn.bing.com"
+desc=""
+re = []
+for sein in Desc:
+        desc=sein
+print(desc)
+
+for  se  in Links:
+        Str = Str+se
+        re = Str.split("&",1)
+        Str=re[0]
+print(Str)
+picfile="pic/"+desc+".jpg"
+picture=requests.get(Str)
+with open (picfile,"wb+") as f:
+        f.write(picture.content)
+print("Get The Picture of Bing Successfully! "+"The Picture in \""+picfile+"\"")
