1c088b68ae9966e8f58601c5f5b0de70c5631c57
lixuanliming <lixuanliming@gmail.com>
2020-06-17 00:28:06 +0800

    add log time

--- Bing_spider.py
+++ Bing_spider.py
@@ -1,7 +1,8 @@
 #coding=utf-8
 import requests
 from lxml import etree
-import os
+import datetime
+
 webPage = requests.get(url = "https://cn.bing.com/?FORM=BEHPTB&ensearch=1",
         headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"}
         )
@@ -15,15 +16,18 @@ desc=""
 re = []
 for sein in Desc:
         desc=sein
+print(datetime.datetime.now())
 print(desc)
 
 for  se  in Links:
         Str = Str+se
         re = Str.split("&",1)
         Str=re[0]
+print(datetime.datetime.now())
 print(Str)
 picfile="pic/"+desc+".jpg"
 picture=requests.get(Str)
 with open (picfile,"wb+") as f:
         f.write(picture.content)
+print(datetime.datetime.now())
 print("Get The Picture of Bing Successfully! "+"The Picture in \""+picfile+"\"")
