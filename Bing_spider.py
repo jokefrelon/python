26e29a44fd17eaac629b28559ac7ea69106308f6
Joker <jokemetoday@outlook.com>
2022-04-21 16:05:14 +0800

    update bing_pic spider

--- Bing_spider.py
+++ Bing_spider.py
@@ -3,30 +3,39 @@ import requests
 from lxml import etree
 import datetime
 
-webPage = requests.get(url = "https://cn.bing.com/?FORM=BEHPTB&ensearch=1",
-        headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"}
-        )
+
+def logout(desc,realink):
+	print("")
+	print(datetime.datetime.now())
+	print(desc)
+
+	print("")
+	print(datetime.datetime.now())
+	print(realink)
+
+def saveimg(desc,realink):
+	picfile="/home/frelon/Bing_pic/pic/"+desc+".jpg"
+	picture=requests.get(realink)
+	with open (picfile,"wb+") as f:
+	         f.write(picture.content)
+
+webPage = requests.get(
+	url = "https://cn.bing.com/?FORM=BEHPTB&ensearch=1",
+	headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"}
+)
 we = webPage.text
 ssr = etree.HTML(we)
 
-# 已弃用 Links = ssr.xpath("//div/div[@id='bgDiv']/div/@data-ultra-definition-src")
-staticLink = ssr.xpath("//div[@class='img_cont']/@style")
-link=staticLink[0].split("(")[1].split(")")[0].split("&")[0].replace("1920x1080","UHD")
-realink="https://cn.bing.com"+link
-# 已弃用 Desc=ssr.xpath("//a/div/h2/text()")
-Description=ssr.xpath("//h1[@class='text']/text()")
-desc=Description[0]
-
-
-print("")
-print(datetime.datetime.now())
-print(desc)
-
-print("")
-print(datetime.datetime.now())
+# image url
+realink=ssr.xpath("//link[@id='preloadBg']/@href")[0].split("&")[0].replace("1920x1080","UHD").replace("s.cn.bing.net","www.bing.com")
 print(realink)
 
-picfile="/home/frelon/Bing_pic/pic/"+desc+".jpg"
-picture=requests.get(realink)
-with open (picfile,"wb+") as f:
-         f.write(picture.content)
+# description 
+desc=ssr.xpath("//meta[@property='og:title']/@content")[0]
+print(desc)
+
+# print log
+logout(desc,realink)
+
+# save image to file
+saveimg(desc,realink)
